ef82111f4bb913e8dda7644124581b3490f404b2
samer-nasr <samernasr1@live.com>
2025-05-29 22:57:48 +0300

    npm

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ composer install
 php artisan key:generate
 php artisan migrate --force
 php artisan config:cache
-php artisan serve --host=0.0.0.0 --port=3000
+php artisan serve --host=0.0.0.0 --port=${PORT:-3000}
